da654c32012545cb6fe9a3c40dd232727b8137a2
gabrielmellace1 <gabrielmellace@gmail.com>
2024-03-12 02:19:19 -0300

    initial commit

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ const http = require('http-server');
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
  app.listen(port, () => console.log(`Server running on port ${port}`));
--- public/app.js
+++ public/app.js
@@ -1,39 +1,28 @@
-const tokenContractAddressETH = "0x808688c820AB080A6Ff1019F03E5EC227D9b522B";
-const tokenContractAddressBlast = "0xb9dfCd4CF589bB8090569cb52FaC1b88Dbe4981F";
-const bridgeContractAddress = "0x697402166Fbf2F22E970df8a6486Ef171dbfc524";
-const blastRPC = "https://rpc.blast.io";
-let accounts, tokenContractETH, tokenContractBlast, bridgeContract, web3Blast;
-
 document.addEventListener('DOMContentLoaded', async () => {
-    console.log("Document loaded. Initializing...");
     await init();
-    await updateBalanceETH();
-    await updateBalanceBlast();
-    await updateAllowance();
 });
 
-
-document.getElementById('maxButton').addEventListener('click', () => {
-    const maxBalance = document.getElementById('tokenBalanceETH').innerText;
-    document.getElementById('amountToBridge').value = maxBalance;
-    updateButtonStates(); // Make sure this function updates the state of the buttons based on the new input value
-});
-
+const oldTokenContractAddress = "0xEE06A81a695750E71a662B51066F2c74CF4478a0";
+const newTokenContractAddress = "0x4b520c812e8430659fc9f12f6d0c39026c83588d";
+let accounts, oldTokenContract, newTokenContract, web3;
 
 async function init() {
     if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        web3Blast = new Web3(new Web3.providers.HttpProvider(blastRPC));
+        web3 = new Web3(window.ethereum);
         try {
-            console.log("Requesting account access...");
             accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
             console.log("Accounts received:", accounts);
-            const tokenABI = await fetch('tokenABI.json').then(response => response.json());
-            tokenContractETH = new web3.eth.Contract(tokenABI, tokenContractAddressETH);
-            tokenContractBlast = new web3Blast.eth.Contract(tokenABI, tokenContractAddressBlast);
-            const bridgeABI = await fetch('bridgeABI.json').then(response => response.json());
-            bridgeContract = new web3.eth.Contract(bridgeABI, bridgeContractAddress);
-            console.log("Contracts initialized.");
+
+            const oldTokenABI = await fetch('oldDG.json').then(response => response.json());
+            oldTokenContract = new web3.eth.Contract(oldTokenABI, oldTokenContractAddress);
+
+            const newTokenABI = await fetch('newDG.json').then(response => response.json());
+            newTokenContract = new web3.eth.Contract(newTokenABI, newTokenContractAddress);
+
+            console.log("Contracts initialized with their respective ABIs.");
+            await updateBalances();
+            await updateAllowance();
+            document.getElementById('amountToBridge').disabled = false;
         } catch (error) {
             console.error("Could not get accounts", error);
         }
@@ -43,64 +32,52 @@ async function init() {
     }
 }
 
-async function updateBalanceETH() {
-    console.log("Updating ETH balance...");
-    const balance = await tokenContractETH.methods.balanceOf(accounts[0]).call();
-    document.getElementById('tokenBalanceETH').innerText = web3.utils.fromWei(balance, 'ether');
-    console.log("ETH Balance updated:", balance);
-}
+async function updateBalances() {
+    const oldBalance = await oldTokenContract.methods.balanceOf(accounts[0]).call();
+    document.getElementById('oldTokenBalance').innerText = web3.utils.fromWei(oldBalance, 'ether');
 
-async function updateBalanceBlast() {
-    console.log("Updating Blast balance...");
-    // Assuming the account address format is compatible with Blast; otherwise, additional steps may be needed.
-    const balance = await tokenContractBlast.methods.balanceOf(accounts[0]).call();
-    document.getElementById('tokenBalanceBlast').innerText = web3Blast.utils.fromWei(balance, 'ether');
-    console.log("Blast Balance updated:", balance);
+    const newBalance = await newTokenContract.methods.balanceOf(accounts[0]).call();
+    document.getElementById('newTokenBalance').innerText = web3.utils.fromWei(newBalance, 'ether');
 }
 
 async function updateAllowance() {
-    console.log("Updating allowance...");
-    const allowance = await tokenContractETH.methods.allowance(accounts[0], bridgeContractAddress).call();
-    //document.getElementById('allowance').innerText = web3.utils.fromWei(allowance, 'ether');
-    console.log("Allowance updated:", allowance);
+    const allowance = await oldTokenContract.methods.allowance(accounts[0], newTokenContractAddress).call();
     updateButtonStates();
 }
 
 async function updateButtonStates() {
-    console.log("Updating button states...");
-    const amountToBridgeETH = document.getElementById('amountToBridge').value || "0";
-    const amountToBridgeWEI = web3.utils.toWei(amountToBridgeETH, 'ether');
-    console.log("Amount to bridge in WEI:", amountToBridgeWEI);
-    const allowanceWEI = await tokenContractETH.methods.allowance(accounts[0], bridgeContractAddress).call();
-    console.log("Current allowance in WEI:", allowanceWEI);
+    if (typeof web3 === 'undefined') {
+        console.error('web3 is not initialized.');
+        return;
+    }
 
-    if (BigInt(amountToBridgeWEI) > 0n && BigInt(allowanceWEI) >= BigInt(amountToBridgeWEI)) {
-        console.log("Enabling bridge button, disabling approve button.");
+    const amountToMigrate = document.getElementById('amountToBridge').value || "0";
+    const amountToMigrateWEI = web3.utils.toWei(amountToMigrate, 'ether');
+    const allowanceWEI = await oldTokenContract.methods.allowance(accounts[0], newTokenContractAddress).call();
+
+    if (BigInt(amountToMigrateWEI) > 0n && BigInt(allowanceWEI) >= BigInt(amountToMigrateWEI)) {
         document.getElementById('approveButton').disabled = true;
-        document.getElementById('bridgeButton').disabled = false;
-    } else if (BigInt(amountToBridgeWEI) > 0n) {
-        console.log("Enabling approve button, disabling bridge button.");
+        document.getElementById('migrateButton').disabled = false;
+    } else if (BigInt(amountToMigrateWEI) > 0n) {
         document.getElementById('approveButton').disabled = false;
-        document.getElementById('bridgeButton').disabled = true;
+        document.getElementById('migrateButton').disabled = true;
     } else {
-        console.log("Disabling both buttons.");
         document.getElementById('approveButton').disabled = true;
-        document.getElementById('bridgeButton').disabled = true;
+        document.getElementById('migrateButton').disabled = true;
     }
 }
 
 document.getElementById('approveButton').addEventListener('click', async () => {
-    const amountToBridgeETH = document.getElementById('amountToBridge').value;
-    const amountToBridgeWEI = web3.utils.toWei(amountToBridgeETH, 'ether');
-    console.log("Approving...", amountToBridgeWEI);
-    await tokenContractETH.methods.approve(bridgeContractAddress, amountToBridgeWEI).send({ from: accounts[0] });
+    const amountToApproveWEI = web3.utils.toWei(document.getElementById('amountToBridge').value, 'ether');
+    console.log(amountToApproveWEI);
+    await oldTokenContract.methods.approve(newTokenContractAddress, amountToApproveWEI).send({ from: accounts[0] });
+    console.log("Approval successful");
     await updateAllowance();
 });
 
-document.getElementById('bridgeButton').addEventListener('click', async () => {
-    const amountToBridgeETH = document.getElementById('amountToBridge').value;
-    const amountToBridgeWEI = web3.utils.toWei(amountToBridgeETH, 'ether');
-    console.log("Bridging...", amountToBridgeWEI);
-    await bridgeContract.methods.bridgeERC20(tokenContractAddressETH, tokenContractAddressBlast, amountToBridgeWEI, '750000', '0x').send({ from: accounts[0] });
-    // After bridging, you might want to update balances or perform other actions
+document.getElementById('migrateButton').addEventListener('click', async () => {
+    const amountToMigrateWEI = web3.utils.toWei(document.getElementById('amountToBridge').value, 'ether');
+    await newTokenContract.methods.goLight(amountToMigrateWEI).send({ from: accounts[0] });
+    console.log("Migration successful");
+    await updateBalances();
 });
